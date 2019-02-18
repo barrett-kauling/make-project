10ffd17b5119e99f62e9f7c53003d8b5d164f5e5
Barrett Kauling <barrett.e.kauling@gmail.com>
2019-02-17 20:38:02 -0800

    Create working template, move source files to /usr/share/make-project/(ln)/

--- make-project.sh
+++ make-project.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-select()
-{
+Select() {
 	case $LANGUAGE in
 		"1")
 			break
@@ -21,9 +20,15 @@ do
 		"中文")
 			PS3="欢迎光临使-项目！"
 			LANGUAGE=1
+			LN="cn"
 			break
 			;;
 		"English")
+			PS3="Welcome to Make-Project!"
+			LANGUAGE=2
+			LN="en"
+			break
+			;;
 		*)
 			PS3="Welcome to Make-Project!"
 			LANGUAGE=2
@@ -33,19 +38,34 @@ do
 done
 echo $PS3
 
+if [ "$(uname -s)" == "MSYS_NT-10.0" ]; then
+	CC_NAME="g++.exe"
+	MSYS2_DIR="$(dirname $(find /c/ -name msys2.exe))"
+	USR="$MSYS2_DIR/usr"
+elif [ "$(uname -s)" == "Linux" ]; then
+	CC_NAME="g++"
+	USR="/usr"
+fi
+
 case $LANGUAGE in
 	"1")
 		PS3="选择编译器》 "
+		break
 		;;
 	"2")
+		PS3="Select compiler> "
+		break
+		;;
 	*)
 		PS3="Select compiler> "
+		break
 		;;
 esac
 
+echo "searching \$PATH for $CC_NAME compilers ..."
 for path in $(echo $PATH | tr ":" "\n")
 do
-	for ccx in $(find $path -name *g++)
+	for ccx in $(find $path -name *$CC_NAME 2> /dev/null)
 	do
 		cc="$cc $ccx:$($ccx -dumpversion)"
 	done
@@ -54,11 +74,10 @@ done
 
 select CC in $cc
 do
-	echo $(echo $CC | awk  -F':' '{print $1}')
+	echo $CC | awk -F':' '{print $1}'
+	echo $CC | awk  -F':' '{print $1}' > /usr/share/make-project/SYS_CC
 	break;
 done
-
-exit
 
 case $LANGUAGE in
 	"1")
@@ -66,12 +85,17 @@ case $LANGUAGE in
 		break
 		;;
 	"2")
+		PS3="Select directory> "
+		break
+		;;
 	*)
 		PS3="Select directory> "
 		break
 		;;
 esac
-read $directory
+read -p "$PS3" wd
+mkdir -p $wd
+stat $wd
 
 case $LANGUAGE in
 	"1")
@@ -79,12 +103,17 @@ case $LANGUAGE in
 		break
 		;;
 	"2")
+		PS3="Project name> "
+		break
+		;;
 	*)
 		PS3="Project name> "
 		break
 		;;
 esac
-read $project_name
+read -p "$PS3" project_name
+echo $project_name
+
 
 prompt="project class exit"
 if  [ $LANGUAGE == "1" ]; then
@@ -93,21 +122,41 @@ elif [ $LANGUAGE == "2" ]; then
 	prompt="project class exit"
 fi
 
+case $LANGUAGE in
+	"1")
+		PS3="菜单》 "
+		break
+		;;
+	"2")	
+		PS3="Menu> "
+		break
+		;;
+	*)
+		PS3="Menu> "
+		break
+		;;
+esac
+
 select OPT in $prompt
 do
 	case $OPT in
 		"项目")
+			mkdir -p $wd/{建设,并把,源码}
+			break
+			;;
 		"project")
-			mkdir -p $directory/{build,include,src}
-			cp -v README.md Makefile project.session SciTE.properties LICENSE $directory
-			cp -v main.cpp $directory/src
+			mkdir -p $wd/{build,include,src}
+			cp -v $USR/share/make-project/en/{README.md,Makefile,project.session,SciTE.properties,LICENSE} $wd
+			cp -v $USR/share/make-project/en/main.cpp $wd/src
 			break
 			;;
 		
 		"课时")
+			break
+			;;
 		"class")
-			cp -v class.h $directory/include
-			cp -v class.cpp $directory/src
+			cp -v $USR/share/make-project/en/class.h $wd/include
+			cp -v $USR/share/make-project/en/class.cpp $wd/src
 			break
 			;;
 		
@@ -115,12 +164,11 @@ do
 			echo "离去"
 			break
 			;;
-			
 		"exit")
 			echo "exit"
 			break
 			;;
-			
+
 		*)
 			break
 			;;
@@ -128,3 +176,5 @@ do
 done
 
 read OPT
+
+exit
